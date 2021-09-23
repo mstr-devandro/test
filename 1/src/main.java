7d6057bbdf3986f35d15a641fead252c9d47e73f
Risa Herdianto <aputra.zha@gmail.com>
2021-09-23 15:46:07 +0700

    generate random array list of char

---
+++ 1/src/main.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class main {
+    public static void main(String[] args) {
+        int n = 30;
+        String AlphaString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        ArrayList<Character> stringArr = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+
+            int index
+                    = (int) (AlphaString.length()
+                    * Math.random());
+
+            Character charString = AlphaString
+                    .charAt(index);
+
+            stringArr.add(charString);
+        }
+
+
+        System.out.println(stringArr);
+
+    }
+}
